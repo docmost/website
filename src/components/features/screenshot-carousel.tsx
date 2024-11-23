a3dacf8565a4218b67cfae40af1659da07e8f208
Philipinho <16838612+Philipinho@users.noreply.github.com>
2024-11-23 18:27:48 +0000

    feat: screenshots gallery

--- src/components/features/screenshot-carousel.tsx
+++ src/components/features/screenshot-carousel.tsx
@@ -1,52 +1,77 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 import { Carousel } from "@mantine/carousel";
 import { Container, Image, Modal, Text, Title } from "@mantine/core";
 import classes from "./features.module.css";
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
-const images = [
-  { url: "/screenshots/home.png", title: "Workspace home" },
-  { url: "/screenshots/editor.png", title: "Editor" },
-  { url: "/screenshots/editor2.png", title: "Editor" },
-  { url: "/screenshots/slash-command.png", title: "Editor slash command" },
-  { url: "/screenshots/page-history.png", title: "Page history" },
-  { url: "/screenshots/search.png", title: "Search" },
-  { url: "/screenshots/space-list.png", title: "Space list" },
-  { url: "/screenshots/space-permissions.png", title: "Space permissions" },
-  { url: "/screenshots/space-overview.png", title: "Space overview" },
-  { url: "/screenshots/member-list.png", title: "Workspace member list" },
+interface ImageProps {
+  url: string;
+  title: string;
+  width: number;
+  height: number;
+}
+const images: ImageProps[] = [
+  { url: "/screenshots/home.png", title: "Workspace home", width: 1200, height: 800 },
+  { url: "/screenshots/editor.png", title: "Editor", width: 1500, height: 800 },
+  { url: "/screenshots/editor2.png", title: "Editor", width: 1500, height: 800 },
+  { url: "/screenshots/slash-command.png", title: "Editor slash command", width: 1500, height: 800 },
+  { url: "/screenshots/page-history.png", title: "Page history", width: 1500, height: 800 },
+  { url: "/screenshots/search.png", title: "Search", width: 1500, height: 800 },
+  { url: "/screenshots/space-list.png", title: "Space list", width: 1200, height: 800 },
+  { url: "/screenshots/space-permissions.png", title: "Space permissions", width: 1500, height: 800 },
+  { url: "/screenshots/space-overview.png", title: "Space overview", width: 1500, height: 800 },
+  { url: "/screenshots/member-list.png", title: "Workspace member list", width: 1500, height: 800 },
 ];
 
 export function ScreenshotCarousel() {
-  const [opened, setOpened] = useState(false);
-  const [selectedImage, setSelectedImage] = useState("");
-  const [selectedTitle, setSelectedTitle] = useState("");
+  const galleryId = "gallery";
 
-  const slides = images.map((image) => (
+  useEffect(() => {
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: '#' + galleryId,
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
+
+    return () => {
+      lightbox.destroy();
+      // @ts-expect-error
+      lightbox = null;
+    };
+  }, []);
+
+  const slides = images.map((image, index) => (
     <Carousel.Slide key={image.url}>
-      <Image
-        className={classes.carouselImage}
-        src={image.url}
-        alt={image.title}
-        h={200}
-        onClick={() => {
-          setSelectedImage(image.url);
-          setSelectedTitle(image.title);
-          setOpened(true);
-        }}
-        style={{ cursor: "pointer" }}
-      />
+      <a
+        href={image.url}
+        data-pswp-width={image.width}
+        data-pswp-height={image.height}
+        key={galleryId + '-' + index}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          className={classes.carouselImage}
+          src={image.url}
+          alt={image.title}
+          h={200}
+          style={{ cursor: 'pointer' }}
+        />
+      </a>
     </Carousel.Slide>
-  ));
+));
 
-  return (
-    <div className={classes.carouselWrapper} id="screenshots">
-      <Title order={2} className={classes.title}>
-        Screenshots
-      </Title>
-      <Carousel
-        classNames={{ indicator: classes.carouselIndicator }}
+return (
+  <div className={classes.carouselWrapper} id="screenshots">
+    <Title order={2} className={classes.title}>
+      Screenshots
+    </Title>
+    <Carousel
+      classNames={{ indicator: classes.carouselIndicator }}
         withIndicators
         height={200}
         maw={900}
@@ -54,20 +79,11 @@ export function ScreenshotCarousel() {
         slideGap={{ base: 0, sm: "md" }}
         loop
         align="start"
+        className="pswp-gallery"
+        id={galleryId}
       >
         {slides}
       </Carousel>
-      <Modal
-        removeScrollProps={{ allowPinchZoom: true }}
-        opened={opened}
-        onClose={() => setOpened(false)}
-        title={selectedTitle}
-        size="auto"
-      >
-        <Container size="lg">
-          <Image src={selectedImage} alt={selectedTitle} />
-        </Container>
-      </Modal>
     </div>
   );
 }
