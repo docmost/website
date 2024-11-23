a9dbc21f517e4430815e8bfecbfd13c60c2bdcf4
Philipinho <16838612+Philipinho@users.noreply.github.com>
2024-11-23 19:38:44 +0000

    feat: revamped footer

--- src/components/shell/footer.tsx
+++ src/components/shell/footer.tsx
@@ -1,64 +1,121 @@
-import { Anchor, Group, ActionIcon, rem } from "@mantine/core";
-import { IconBrandGithub, IconBrandTwitter } from "@tabler/icons-react";
+import { Group, ActionIcon, Container, Text } from "@mantine/core";
+import {
+  IconBrandGithubFilled,
+  IconBrandLinkedinFilled,
+  IconBrandTwitterFilled,
+} from '@tabler/icons-react';
 import classes from "./footer.module.css";
-import { AppLink } from "@/libs/config";
+import { AppLink } from '@/libs/config';
 
-const links = [
-  { link: AppLink.Github, label: "GitHub" },
+interface LinkGroup {
+  title: string;
+  links: Link[];
+}
 
-  { link: "/docs", label: "Documentation" },
-  { link: AppLink.Blog, label: "Blog" },
+interface Link {
+  label: string;
+  link: string;
+  isInternal?: boolean;
+}
+
+const data: LinkGroup[] = [
+  {
+    title: 'About',
+    links: [
+      { label: 'Features', link: '#features', isInternal: true },
+      { label: 'Contact us', link: 'mailto:hello@docmost.com', isInternal: true },
+    ],
+  },
+  {
+    title: 'Project',
+    links: [
+      { label: 'Documentation', link: AppLink.Documentation },
+      { label: 'Releases', link: AppLink.Releases },
+      { label: 'Blog', link: AppLink.Blog },
+    ],
+  },
+  {
+    title: 'Community',
+    links: [
+      { label: 'Discussion (Forum)', link: AppLink.Discussion },
+      { label: 'Follow on Twitter (X)', link: AppLink.Twitter },
+      { label: 'LinkedIn', link: AppLink.LinkedIn },
+    ],
+  },
 ];
 
 export function Footer() {
-  const items = links.map((link) => (
-    <Anchor
-      c="dimmed"
-      key={link.label}
-      href={link.link}
-      target="_blank"
-      lh={1}
-      size="sm"
-    >
-      {link.label}
-    </Anchor>
-  ));
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<'a'>
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        target={link.isInternal ? '_self': '_blank'}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
 
   return (
-    <div className={classes.inner}>
-      <div></div>
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          <Text size="xs" c="dimmed" className={classes.description}>
+            An open-source collaborative wiki and documentation software
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © {new Date().getFullYear()}  Docmost. All rights reserved.
+        </Text>
 
-      <Group className={classes.links}>{items}</Group>
+        <Group gap={8} className={classes.social} justify="flex-end" wrap="nowrap">
+          <ActionIcon
+            size="lg"
+            variant="default"
+            radius="xl"
+            component="a"
+            href="https://github.com/docmost/docmost"
+            target="_blank"
+          >
+            <IconBrandGithubFilled size={18} />
+          </ActionIcon>
 
-      <Group gap="xs" justify="flex-end" wrap="nowrap">
-        <ActionIcon
-          size="lg"
-          variant="default"
-          radius="xl"
-          component="a"
-          href="https://github.com/docmost/docmost"
-          target="_blank"
-        >
-          <IconBrandGithub
-            style={{ width: rem(18), height: rem(18) }}
-            stroke={1.5}
-          />
-        </ActionIcon>
+          <ActionIcon
+            size="lg"
+            variant="default"
+            radius="xl"
+            component="a"
+            href="https://twitter.com/docmostHQ"
+            target="_blank"
+          >
+            <IconBrandTwitterFilled size={18} />
+          </ActionIcon>
 
-        <ActionIcon
-          size="lg"
-          variant="default"
-          radius="xl"
-          component="a"
-          href="https://twitter.com/docmostHQ"
-          target="_blank"
-        >
-          <IconBrandTwitter
-            style={{ width: rem(18), height: rem(18) }}
-            stroke={1.5}
-          />
-        </ActionIcon>
-      </Group>
-    </div>
+          <ActionIcon
+            size="lg"
+            variant="default"
+            radius="xl"
+            component="a"
+            href="https://www.linkedin.com/company/104338375"
+            target="_blank"
+          >
+            <IconBrandLinkedinFilled size={18} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
   );
 }
--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,7 +1,10 @@
 export const AppLink = {
   Github: "https://github.com/docmost/docmost",
   Documentation: "https://docmost.com/docs",
+  Releases: "https://github.com/docmost/docmost/releases",
+  Discussion: "https://github.com/docmost/docmost/discussions",
   Demo: "https://demo.docmost.com",
   Blog: "https://docmost.com/blog",
   Twitter: "https://twitter.com/docmostHQ",
+  LinkedIn: "https://www.linkedin.com/company/104338375",
 };
